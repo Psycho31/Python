7d3001d2ed00343c57960e4fc4019f74ae4a0d45
Psycho31 <49193993+Psycho31@users.noreply.github.com>
2019-07-02 16:06:45 +0300

    Update Cisco Manage.py

--- Cisco Manage.py
+++ Cisco Manage.py
@@ -4,159 +4,178 @@ import sys
 
 USER = input("Username: ")
 PASSWORD = getpass.getpass()
-print("""
-1 floor - 172.16.2.6
-2 floor - 172.16.2.4
-Adushkin plaza - 172.16.2.5
-""")
 
-DEVICES_IP = input("Enter the ip address: ")
-DEVICE_PARAMS = {'device_type': 'cisco_ios',
-                 'ip': DEVICES_IP,
-                 'username': USER,
-                 'password': PASSWORD, }
+DEVICE = ''
 
-ssh = ConnectHandler(**DEVICE_PARAMS)
-
-number = ''
-
-while number != '6':
+while DEVICE != '4':
     print("""
-    1. Change the number of the maximum mac-adresses
-    2. Disable port
-    3. Port info
-    4. Transplant 2 users
-    5. Change PC(1 user)
-    6. Quit
+    1. Первый этаж
+    2. Второй этаж
+    3. Адушкин плаза
+    4. Выход
     """)
-
-    number = int(input("what do you want to do?: "))
-
-    if number == 1:
-        INVENTORY = input("enter the inventory of the pc: ")
-        PORT = ssh.send_command("sh int status | i " + INVENTORY)
-        PORT = PORT.split()
-        PORT = PORT[0]
-        PORT = 'interface ' + PORT
-        SHOW = ssh.send_command("sh run " + PORT)
-        print(SHOW)
-        print(input("press any key"))
-        MAX_MAC = input("enter maximum count of devices : ")
-        COMMANDS = [PORT,
-                "switchport port-security maximum " + MAX_MAC]
-        RESULT = ssh.send_config_set(COMMANDS)
-        SHOW = ssh.send_command("sh run " + PORT)
-        print(SHOW)
-        print(input("press any key"))
-
-
-    elif number == 2:
-        INVENTORY = input("enter the inventory of the pc: ")
-        PORT = ssh.send_command("sh int status | i " + INVENTORY)
-        PORT = PORT.split()
-        PORT = PORT[0]
-        PORT = "interface " + PORT
-        SHOW = ssh.send_command("sh run " + PORT)
-        print(SHOW)
-        print(input("press any key"))
-        NEW_INVENTORY = input("enter the new description of " + PORT + ":")
-        clear_port = ssh.send_command("clear port-sec sti " + PORT)
-        commands = [PORT,
-                "shutdown",
-                "description " + NEW_INVENTORY]
-        RESULT = ssh.send_config_set(commands)
-        SHOW = ssh.send_command("sh run " + PORT)
-        print(SHOW)
-        print(input("end. press any key"))
-
-
-    elif number == 3:
-        INVENTORY_1 = input("enter the inventory of the PC: ")
-        PORT = ssh.send_command("sh int status | i " + INVENTORY_1)
-        PORT = PORT.split()
-        PORT = PORT[0]
-        PORT = 'interface ' + PORT
-        SHOW = ssh.send_command("sh run " + PORT)
-        print(SHOW)
-
-        print(input("press any key"))
-
-
-    elif number == 4:
-        INVENTORY_1 = input("enter inventory of 1st PC: ")
-        PORT_1 = ssh.send_command("sh int status | i " + INVENTORY_1)
-        PORT_1 = PORT_1.split()
-        PORT_1 = PORT_1[0]
-        PORT_1 = 'interface ' + PORT_1
-        SHOW_1 = ssh.send_command("sh run " + PORT_1)
-        print(SHOW_1)
-        print(input("press any key"))
-        NEW_INVENTORY_1 = input("enter new description of " + PORT_1 + ":")
-        COMMANDS_PORT_1 = [PORT_1,
-                       "description " + NEW_INVENTORY_1,
-                       "no switchport port-security",
-                       "no shutdown"]
-        RESULT_1 = ssh.send_config_set(COMMANDS_PORT_1)
-        SHOW_1 = ssh.send_command("sh run " + PORT_1)
-        print(SHOW_1)
-        print(input("press any key"))
-
-        INVENTORY_2 = input("enter inventory of 2nd PC: ")
-        PORT_2 = ssh.send_command("sh int status | i " + INVENTORY_2)
-        PORT_2 = PORT_2.split()
-        PORT_2 = PORT_2[0]
-        PORT_2 = 'interface ' + PORT_2
-        SHOW_2 = ssh.send_command("sh run " + PORT_2)
-        print(SHOW_2)
-        print(input("press any key"))
-        NEW_INVENTORY_2 = input("enter new description of 2nd port: ")
-        COMMANDS_PORT_2 = [PORT_2,
-                       "description " + NEW_INVENTORY_2,
-                       "no switchport port-security",
-                       "no shutdown"]
-        RESULT_2 = ssh.send_config_set(COMMANDS_PORT_2)
-        SHOW_2 = ssh.send_command("sh run " + PORT_2)
-        print(SHOW_2)
-        print(input("press any key"))
-
-        PORT_TEMP = ssh.send_command("sh int status | i temp")
-        PORT_TEMP = PORT_TEMP.split()
-        PORT_TEMP = PORT_TEMP[0]
-        PORT_TEMP = 'interface ' + PORT_TEMP
-        NEW_INVENTORY_TEMP = input("enter new description of temp port: " + PORT_TEMP + ":")
-        COMMANDS_PORT_TEMP = [PORT_TEMP,
-                          "description " + NEW_INVENTORY_TEMP,
-                          "no switchport port-security",
-                          "no shutdown"]
-        REULST_TEMP = ssh.send_config_set(COMMANDS_PORT_TEMP)
-        SHOW_TEMP = ssh.send_command("sh run " + PORT_TEMP)
-        print(SHOW_TEMP)
-        print(input("end. press any key"))
-
-    elif number == 5:
-        INVENTORY = input("enter the inventory of the pc: ")
-        PORT = ssh.send_command("sh int status | i " + INVENTORY)
-        PORT = PORT.split()
-        PORT = PORT[0]
-        PORT = 'interface ' + PORT
-        SHOW = ssh.send_command("sh run " + PORT)
-        print(SHOW)
-        print(input("press any key"))
-        clear_port = ssh.send_command("clear port-sec sti " + PORT)
-        NEW_INVENTORY = input("enter the new description: ")
-        COMMANDS = [PORT,
-                "no shutdown",
-                "description " + NEW_INVENTORY]
-        RESULT = ssh.send_config_set(COMMANDS)
-        SHOW = ssh.send_command("sh run " + PORT)
-        print(SHOW)
-        print(input("press any key"))
-
+    DEVICE = (int(input('Выберите усройство: ')))
+    if DEVICE ==1:
+        DEVICES_IP = "172.16.2.6"
+        DEVICE_PARAMS = {'device_type': 'cisco_ios',
+                        'ip': DEVICES_IP,
+                        'username': USER,
+                        'password': PASSWORD, }
+        ssh = ConnectHandler(**DEVICE_PARAMS)
+    elif DEVICE == 2:
+        DEVICES_IP = "172.16.2.4"
+        DEVICE_PARAMS = {'device_type': 'cisco_ios',
+                         'ip': DEVICES_IP,
+                         'username': USER,
+                         'password': PASSWORD, }
+        ssh = ConnectHandler(**DEVICE_PARAMS)
+    elif DEVICE == 3:
+        DEVICES_IP = "172.16.2.5"
+        DEVICE_PARAMS = {'device_type': 'cisco_ios',
+                         'ip': DEVICES_IP,
+                         'username': USER,
+                         'password': PASSWORD, }
+        ssh = ConnectHandler(**DEVICE_PARAMS)
+    elif DEVICE == 4:
+        break
     else:
         print("""
-    wrong number
-      """)
-print(input("press any key"))
+        Нет такого :)
+        """)
+        continue
+
+
+    def port_discovery():
+        PORT_LOOKING = ssh.send_command("sh int status | i " + INVENTORY)
+        PORT_LOOKING = PORT_LOOKING.split()
+        PORT_LOOKING = PORT_LOOKING[0]
+        PORT_LOOKING = 'interface ' + PORT_LOOKING
+        SHOW = ssh.send_command("sh run " + PORT_LOOKING)
+        print(SHOW)
+        return PORT_LOOKING
+        # ищет порт по инвентарнику и выводит его на экран
+
+
+    number = ''
+
+    while number != '6':
+        print("""
+        1. Изменить количество разрешенных mac-адресов на порту
+        2. Выключить порт
+        3. Вывести информацию о настройках порта
+        4. Включить порт        
+        5. Поменять ПК/Телефон на порту
+        6. Вернуться в меню выбора устройств
+        """)
+
+        number = int(input("Что вы хотите сделать?: "))
+
+        if number == 1:
+            INVENTORY = input("Введите инвентарный номер устройства(только цифры): ")
+            PORT = port_discovery()
+            MAX_MAC = input("Введите количество разрешенных mac-адресов на порту : ")
+            COMMANDS = [PORT,
+                        "switchport port-security maximum " + MAX_MAC]
+            RESULT = ssh.send_config_set(COMMANDS)
+            SHOW = ssh.send_command("sh run " + PORT)
+            print(SHOW)
+            print(input("Нажмите Enter"))
+
+        elif number == 2:
+            print("""
+            
+            ----------------------
+            Очищает port-security.
+            Убирает описание.
+            Выключает PoE.
+            Выключает сам порт.
+            ----------------------
+            
+            """)
+            INVENTORY = input("Введите инвентарный номер устройства(только цифры): ")
+            PORT = port_discovery()
+            CLEAR_PORT = ssh.send_command("clear port-sec sti " + PORT)
+            COMMANDS = [PORT,
+                        "switchport port-security maximum 1",
+                        "no description",
+                        "power inline never",
+                        "shutdown"]
+            RESULT = ssh.send_config_set(COMMANDS)
+            SHOW = ssh.send_command("sh run " + PORT)
+            print(SHOW)
+            print(input("Нажмите Enter"))
+
+        elif number == 3:
+            INVENTORY = input("Введите инвентарный номер устройства(только цифры): ")
+            port_discovery()
+            print(input('Нажмите Enter'))
+
+        elif number == 4:
+            INVENTORY = input("Введите номер порта(Например: 1/0/1): ")
+            PORT = "interface g" + INVENTORY
+            SHOW = ssh.send_command("sh run " + PORT)
+            print(SHOW)
+            print(input('Нажите Enter'))
+            MAX_MAC = input('Введите количество разрешенных mac-адресов на порту: ')
+            DESC = input('Введите описание(инвентарный номер устройства): ')
+            PoE = ()
+            while PoE != 'yes' and 'no':
+                PoE = (str(input('Нужно ли PoE на порту?(yes/no): ')))
+                if PoE == 'no':
+                    POWER = "power inline never"
+                elif PoE == 'yes':
+                    POWER = "power inline static max 4000"
+                else:
+                    print('Пожалуйста, введете yes или no')
+                    continue
+
+
+            COMMANDS = [PORT,
+                        "switchport mode access",
+                        "switchport access vlan 100",
+                        "switchport voice vlan 112",
+                        "switchport nonegotiate",
+                        "description " + DESC,
+                        "switchport port-security mac-address sticky",
+                        "switchport port-security maximum " + MAX_MAC,
+                        "switchport port-security",
+                        "ip arp inspection limit rate 100",
+                        "spanning-tree bpduguard enable",
+                        "no shutdown",
+                        POWER]
+            RESULT = ssh.send_config_set(COMMANDS)
+            SHOW = ssh.send_command("sh run " + PORT)
+            print(SHOW)
+            print(input("Нажмите Enter"))
+
+        elif number == 5:
+            print("""
+            
+            ------------------------------------
+            Очищает port-security.
+            Запрашивает ввод описания для порта.
+            ------------------------------------
+                                         
+            """)
+            INVENTORY = input("Введите инвентарный номер устройства(только цифры): ")
+            PORT = port_discovery()
+            clear_port = ssh.send_command("clear port-sec sti " + PORT)
+            DESC = input('Введите описание для порта: ')
+            COMMANDS = [PORT,
+                        "description " + DESC,
+                        ]
+            RESULT = ssh.send_config_set(COMMANDS)
+            SHOW = ssh.send_command("sh run " + PORT)
+            print(SHOW)
+            print(input("Нажмите Enter"))
+        elif number == 6:
+            break
+
+        else:
+            print("""
+            Нет такого :)
+            """)
+
 
 
 
